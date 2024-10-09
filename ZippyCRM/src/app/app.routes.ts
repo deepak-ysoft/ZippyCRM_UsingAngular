54d27f7cfa981a05e318bde43a906bfb60259ab1
deepak-ysoft <deepaksysoft@gmail.com>
2024-10-09 12:48:16 +0530

    login html setup

--- ZippyCRM/src/app/app.routes.ts
+++ ZippyCRM/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //defualt route
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
